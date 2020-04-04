15a11b3fc3243aa8966cd438e98cb2162b152c82
derrickip34 <derrickip34@gmail.com>
2020-04-04 19:39:30 +0300

    Redirect to home if path is empty

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -8,6 +8,7 @@ import { SearchComponent } from '../search/search.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'search', component: SearchComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
