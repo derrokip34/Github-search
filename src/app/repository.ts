040abfa49508aee19f4081551b4e5bc4adf69671
derrickip34 <derrickip34@gmail.com>
2020-04-04 17:52:12 +0300

    Show date repository was create and updated

--- src/app/repository.ts
+++ src/app/repository.ts
@@ -3,5 +3,7 @@ export class Repository {
         public repo_name: string,
         public description: string,
         public html_url: string,
+        public created_at: string,
+        public updated_at: string,
     ) {}
 }
