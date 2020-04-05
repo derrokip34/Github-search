f4a1bcb37cb7f7e490d4c3122e5bb0dc4b4a7148
derrickip34 <derrickip34@gmail.com>
2020-04-05 20:07:12 +0300

    Highlight repository red when hovering mouse above it

--- src/app/highlight-repo.directive.ts
+++ src/app/highlight-repo.directive.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightRepo]'
 })
 export class HighlightRepoDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef) {}
+
+  @HostListener('mouseenter') onMouseEnter(){
+    this.highlight('red');
+  }
+
+  @HostListener('mouseleave') onMouseLeave(){
+    this.highlight(null);
+  }
+
+  private highlight(color: string){
+    this.el.nativeElement.style.backgroundColor = color;
+  }
 
 }
