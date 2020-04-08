8d0627a78de629e0e998eeea9402bbe94a2eff18
derrickip34 <derrickip34@gmail.com>
2020-04-08 14:18:34 +0300

    Delete search repo pipe

--- src/app/search-repo.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SearchRepoPipe } from './search-repo.pipe';
-
-describe('SearchRepoPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SearchRepoPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
